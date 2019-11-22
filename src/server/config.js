25eef62428a269492c6ac9e3a71607158bc179db
VladMakar <vlad@cratustech.com>
2019-11-21 19:57:02 -0800

    removed config from repo

--- src/server/config.js
+++
@@ -1,9 +0,0 @@
-const aws_local_config = 
-{
-    region: "us-west-1",
-    accessKeyId: "695435062401",
-    secretAccessKey: "FHilrLfaQvy8Ss6oRv+bCZsPNuaaiautZjNN+Ihc",
-    endpoint: 'http://localhost:8000'
-};
-
-module.exports = { aws_local_config: aws_local_config };
